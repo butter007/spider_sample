9ec78372e0fe3cb55bd5c75aa8bef5fdf0f974f3
butter007 <butter_007@163.com>
2020-12-17 11:06:57 +0800

    third commit

--- requests_bs4.py
+++ requests_bs4.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-proxies = {"http":"http://193.38.51.182:55555"}
-res = requests.get("http://xiachufang.com",proxies=proxies)
+proxies = {"http": "http://193.38.51.182:55555"}
+res = requests.get("http://xiachufang.com", proxies=proxies)
 
-soup = BeautifulSoup(res.text,"lxml")
+soup = BeautifulSoup(res.text, "lxml")
 print(soup.prettify())
-
