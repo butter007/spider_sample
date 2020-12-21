f84850f4810ce21f23af8746f0540dec415ccb91
butter007 <butter_007@163.com>
2020-12-21 18:25:37 +0800

    整合更改

--- spider_xiachufang.py
+++ spider_xiachufang.py
@@ -1,31 +1,45 @@
 import requests
-
 import os
-from lxml import html
+from lxml import etree
 
 
-def get_pic(url, headers, proxies):
-    etree = html.etree
-    filepath = "D:\\xiachufang"
+# 请求url获取结果
+def fetch(url, headers, proxies):
+    res = requests.get(url, headers, proxies)
+    if res.status_code != 200:
+        res.raise_for_status()
+    return res.text
+
+
+# 根据大类获取每一类的页面加入links
+def getpages(str):
+    numbers = [40076, 40077, 40078, 51848, 52354, 51743, 52351, 51940]
+    pages = []
+    for number in numbers:
+        print(number)
+        for page in range(1, 26):
+            url = "%s%s/?page=%s" % (url_start, number, page)
+            pages.append(url)
+    return pages
+
+
+def getimglinks(pageurl, headers, proxies):
+    li = []
+    pagecon = fetch(pageurl, headers, proxies)
+    selector = pagecon.xpath("//div[@class='cover pure-u']/img/@data-src")
+    for item in selector:
+        li.append(item.split('?')[0])
+    return li
+
+
+# 下载页面
+def download_pic(imgurl, headers, proxies, filepath):
     if not os.path.exists(filepath):
         os.makedirs(filepath)
-    for i in range(1, 26):
-        res = requests.get(url, headers=headers, proxies=proxies)
-
-        htmlcon = etree.HTML(res.text)
-
-        li = []
-
-        re = htmlcon.xpath("//div[@class='cover pure-u']/img/@data-src")
-        for item in re:
-            li.append(item.split('?')[0])
-
-        for item in li:
-            r = requests.get(item)
-            with open(os.path.join(filepath, item.split("/")[-1]), "wb") as fp:
-                for chunk in r.iter_content(chunk_size=1024):
-                    fp.write(chunk)
-        print("第 %s 页" % i)
+    r = requests.get(imgurl, headers, proxies)
+    with open(os.path.join(filepath, imgurl.split("/")[-1]), "wb") as fp:
+        for chunk in r.iter_content(chunk_size=1024):
+            fp.write(chunk)
 
 
 if __name__ == "__main__":
@@ -36,9 +50,8 @@ if __name__ == "__main__":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
         "Referer": "https://pos.baidu.com/"
     }
-    numbers = [40076, 40077, 40078, 51848, 52354, 51743, 52351, 51940]
-    for number in numbers:
-        print(number)
-        for page in range(1, 26):
-            url = "%s%s/?page=%s" % (url_start, number, page)
-            get_pic(url, headers=headers, proxies=proxies)
+    filepath = "D:\\xiachufang"
+    links = []
+    for item in getpages(url_start):
+        links.extend(getimglinks(item, headers, proxies))
+    print(links)
